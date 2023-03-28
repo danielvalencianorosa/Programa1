c96609cc264100f725bccedf0e873f87d32bbd93
Daniel Valenciano <danielvalencianorosa@gmail.com>
2023-03-28 12:27:19 +0200

    first commit

---
+++ src/hola/mundo/Programa1.java
@@ -0,0 +1,15 @@
+
+package hola.mundo;
+
+public class Programa1 {
+    
+   public static void main(String[] args) {
+
+        byte edad = 10;
+      
+        System.out.println("la edad es: " + edad);
+   
+  }
+
+}
+
