bdb575c5b65c10a7252b73eb15b88550dd2ee2f9
danielvalencianorosa <danielvalencianorosa@gmail.com>
2023-03-29 13:38:50 +0200

    Mi primer commit

--- src/hola/mundo/Programa1.java
+++ src/hola/mundo/Programa1.java
@@ -4,11 +4,96 @@ package hola.mundo;
 public class Programa1 {
     
    public static void main(String[] args) {
-
-        byte edad = 10;
-      
-        System.out.println("la edad es: " + edad);
+       
+       //int pi = 3.14;
+       
+      // double pi = 3.141592653;
+                    
+        //System.out.println("The value of pi is: " + pi);
    
+        //int value = 10;
+        //System.out.println(value);
+        //value= 4;
+        //System.out.println(value);
+        
+        //int flag;
+        //flag = 42;
+        
+        //String flag = "Catalunya";
+        
+        //int value = 10;
+        //flag = value;
+        
+        //System.out.println("Pais = " + flag);
+ 
+        //double floatingPoint = 0.42;
+        //floatingPoint = 1;
+        //int value = 10;
+        //floatingPoint = value;
+         //System.out.println("floatingPoint = " + floatingPoint);       
+        
+        //int value = 5;
+        //double floatingPoint = 0.42;
+        //value = floatingPoint;        
+        //System.out.println("value = " + value);
+        
+        //int first = 2;
+        //System.out.println(first);
+        //int second = 4;
+        //System.out.println(second);
+        //int sum = first + second;
+        //System.out.println("sum: " + sum);
+        
+        //System.out.println("cuatro: " (2 + 2));
+        
+        //System.out.println("Cuatro: "+ (2 + 2));
+        //System.out.println("veintidos: "+ 2 + 2);
+        
+        //int $x;
+        //int 123;
+        //int _123;
+        //int #dim;
+        //int %percent;
+        //int *divide;
+        //int central_sales_region_Summer_2005_gross_sales;
+
+        
+       // public static void main (String [] args) {
+       //int a, b, c = 0;
+       //int a, b, c;
+       //int g, int h, int i = 0;
+       //int d, e, F;
+       //int k, l, m; = 0;
+        //}
+        
+        //byte a = 128;
+        
+        //byte b = 100;
+        //b *=1;
+        //byte b = 1;
+        //b = b+1;
+        //int i= 128;
+        
+        
+        //int edad= 30;
+        //System.out.println("edad = " + edad);
+        //edad++;
+        //System.out.println("edad = " + edad);
+        
+         //int b = 4;
+         //b-- ;
+         //System.out.println(--b);
+         //System.out.println(b);        
+        
+        
+        System.out.println("Result = " + 2+3+5);
+        System.out.println("Result = " + 2+3*5);
+        
+        
+        
+        
+        
+        
   }
 
 }
